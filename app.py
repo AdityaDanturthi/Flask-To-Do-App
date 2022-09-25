0ae3713fd0f3360eed20d77f9de4f310bf7d497f
AdityaDanturthi <aditya2096@gmail.com>
2022-09-25 21:15:36 -0230

    created database

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
+from datetime import datetime
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABSE_URI'] = 'sqlite:///tasks.db' # three forward slashes is a relative path and four is for absolute path
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key =True)
+    task = db.Column(db.String(200), nullable = False)
+    dateCreated = dc.Column(db.DateTime, default = datetime.utcnow)
 
 @app.route('/')
 def index():
