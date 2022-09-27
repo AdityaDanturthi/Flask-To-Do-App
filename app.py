fa8535daa8eb95f962d9a99acedce6ce30c4166b
AdityaDanturthi <aditya2096@gmail.com>
2022-09-27 20:20:00 -0230

    able to add tasks

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from flask import Flask, render_template
+from urllib import request
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -10,15 +11,29 @@ db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key =True)
-    task = db.Column(db.String(200), nullable = False)
+    content = db.Column(db.String(200), nullable = False)
+    completed = db.Column(db.Integer, default = 0)
     dateCreated = db.Column(db.DateTime, default = datetime.utcnow)
 
-def __repr__(self):
-    return '<Task %r>' % self.id
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 @app.route('/', methods = ['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        taskContent = request.form['content']
+        newTask = Todo(content = taskContent)
+
+        try:
+            db.session.add(newTask)
+            db.session.commit()
+            return redirect('/')
+        
+        except:
+            return ' There was issue adding your task!'
+    else:
+        tasks = Todo.query.order_by(Todo.dateCreated).all()
+    return render_template('index.html', tasks = tasks)
 
 if __name__ == "__main__":
     app.run(debug = True)
